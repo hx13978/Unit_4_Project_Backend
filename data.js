800200647ca05ff1883e249bf2a813ea2dca1294
Troy Clarke <HX13978@wdxhx13978vcg.home.network>
2021-03-17 10:31:53 -0500

    Updates

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 const list = [
     {
-        date: '03/15/21',
+        date: '15/03/21',
         name:'Amy',
         activity: 'Dance',
         time: '5-6pm'
     },
     {
-        date: '03/15/21',
+        date: '15/03/21',
         name:'Carol',
         activity: 'Dance',
         time: '6-7pm'
     },
     {
-        date: '03/15/21',
+        date: '15/03/21',
         name:'Tom',
         activity: 'Dr. Appt',
         time: '4-5pm'
